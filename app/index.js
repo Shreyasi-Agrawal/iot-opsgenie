8f2e8dd506aae81d7744fd2bfbb19ff038c60980
Nakul Mohan <nmohan@atlassian.com>
2020-04-16 17:01:28 +0530

    Add delay, fix issues

--- app/index.js
+++ app/index.js
@@ -1,6 +1,10 @@
-import { display } from "display";
+import {
+  display
+} from "display";
 import document from "document";
-import { HeartRateSensor } from "heart-rate";
+import {
+  HeartRateSensor
+} from "heart-rate";
 import * as messaging from "messaging";
 
 const hrmLabel = document.getElementById("hrm-label");
@@ -8,51 +12,79 @@ const hrmData = document.getElementById("hrm-data");
 const sensors = [];
 const THRESHOLD = 120;
 
-if (HeartRateSensor) {
-  const hrm = new HeartRateSensor({ frequency: 1 });
-  hrm.addEventListener("reading", () => {
-    hrmData.text = JSON.stringify({
-      heartRate: hrm.heartRate ? hrm.heartRate : 0
+function fetchHeartRate() {
+  if (HeartRateSensor) {
+    const hrm = new HeartRateSensor({
+      frequency: 1
     });
-    if(hrm.heartRate > THRESHOLD) {
-      raiseAlarm();
-    }
-  });
-  sensors.push(hrm);
-  hrm.start();
-} else {
-  hrmLabel.style.display = "none";
-  hrmData.style.display = "none";
+    hrm.addEventListener("reading", () => {
+      console.log("Reading " + hrm.heartRate);
+      hrmData.text = JSON.stringify({
+        heartRate: hrm.heartRate ? hrm.heartRate : 0
+      });
+      if (hrm.heartRate > THRESHOLD) {
+        raiseAlarm(hrm.heartRate);
+      }
+      hrm.stop();
+    });
+    sensors.push(hrm);
+    hrm.start();
+  } else {
+    hrmLabel.style.display = "none";
+    hrmData.style.display = "none";
+  }
 }
+
 
 display.addEventListener("change", () => {
   // Automatically stop all sensors when the screen is off to conserve battery
   display.on ? sensors.map(sensor => sensor.start()) : sensors.map(sensor => sensor.stop());
 });
 
-function raiseAlarm() {
+function raiseAlarm(hrm) {
   // Listen for the onopen event
-messaging.peerSocket.onopen = function() {
-  // Ready to send messages
-  sendMessage();
+  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+    // Send the data to peer as a message
+    console.log("Sending message");
+    sendMessage(hrm);
+  }
+  if (messaging.peerSocket.readyState === messaging.peerSocket.CLOSED) {
+    messaging.peerSocket.onopen = function() {
+      // Ready to send messages
+      console.log("Sending message");
+      sendMessage(hrm);
+    }
+  }
+
 };
-  
-  // Listen for the onerror event
+
+// Listen for the onerror event
 messaging.peerSocket.onerror = function(err) {
   // Handle any errors
   console.log("Connection error: " + err.code + " - " + err.message);
 }
-  
-}
+
 
 // Send a message to the peer
-function sendMessage() {
+function sendMessage(hrm) {
   var data = {
-    hr: THRESHOLD
+    hr: hrm
   }
-
+  console.log("HRM - ", JSON.stringify(data));
   if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
     // Send the data to peer as a message
     messaging.peerSocket.send(data);
   }
 }
+
+// Listen for the onmessage event
+messaging.peerSocket.onmessage = function(message) {
+  console.log("Initiating heart rate in device");
+  if(message.fetch == true) {
+    fetchHeartRate();
+  }
+}
+
+//fetchHeartRate();
+
+setInterval(fetchHeartRate, 1000 * 10);
--- companion/index.js
+++ companion/index.js
@@ -1,6 +1,33 @@
 import * as messaging from "messaging";
 import { settingsStorage } from "settings";
+const MILLISECONDS_PER_MINUTE = 1000 * 10;
+import { me as companion } from "companion";
 
+// Tell the Companion to wake after 30 minutes
+//companion.wakeInterval = MILLISECONDS_PER_MINUTE;
+
+// Listen for the event
+//companion.addEventListener("wakeinterval", fetchHeartRateData);
+
+function fetchHeartRateData() {
+ // Listen for the onopen event
+   let message = {
+      fetch : true
+   };
+	if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+		// Send the data to peer as a message
+
+		console.log("Initiating heart rate from companion");
+		sendMessage(message);
+	}
+	if (messaging.peerSocket.readyState === messaging.peerSocket.CLOSED) {
+		messaging.peerSocket.onopen = function() {
+			// Ready to send messages
+  		console.log("Initiating heart rate from companion");
+			sendMessage(message);
+		}
+	}
+}
 // Create Opsgenie Alarm
 function createAlarm(data)  {
   let accessToken = "223d9c3a-7878-487c-8440-f8d6dbf8e34f";
@@ -9,13 +36,14 @@ function createAlarm(data)  {
     "Authorization": `GenieKey ${accessToken}`
   };
   let body = {
-    "message" : "Heart rate has exceeded 123",
+    "message" : "Heart rate has exceeded " + data.hr,
     "responders" : [
        {
             "name":"Movies",
             "type":"team"
        }
-    ]
+    ],
+    "tags": ["team-a"]
   };
   let url = "https://api.opsgenie.com/v2/alerts";
   console.log("Headers " + JSON.stringify(headers));
@@ -35,5 +63,6 @@ function createAlarm(data)  {
 // Listen for the onmessage event
 messaging.peerSocket.onmessage = function(evt) {
   // Output the message to the console
-  createAlarm(evt)
+  console.log("EVT " + JSON.stringify(evt.data));
+  createAlarm(evt.data)
 }
